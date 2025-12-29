e0ba718fd829c18c8cc07214f7b6e46ba4bcf087
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2025-12-29 21:44:15 +0530

    phase 1 coding completed

--- server/controllers/article.controller.js
+++ server/controllers/article.controller.js
@@ -1,6 +1,7 @@
 const articleModel = require("../models/article.model");
 const scrapeBeyondChats = require("../utils/scraper");
 
+// initial scraping or seeding the db
 exports.initializeDatabase = async (req, res) => {
   try {
     // validating
@@ -46,6 +47,86 @@ exports.initializeDatabase = async (req, res) => {
     console.log("Error while initializing the DB: ", err.message);
     res.status(500).json({
       error: err.message,
+    });
+  }
+};
+
+// get all the articles
+exports.getAllArticles = async (req, res) => {
+  try {
+    const articles = await articleModel.find().sort({ createdAt: -1 });
+
+    if (!articles) {
+      return res.status(404).json({
+        message: "Did not find the articles",
+      });
+    }
+
+    return res.status(200).json({
+      articles,
+      message: "fetched articles",
+    });
+  } catch (err) {
+    console.log("Error while Fetching all the articles: ", err.message);
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+exports.updateArticleById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const article = await articleModel.findById(id);
+    if (!article) {
+      return res.status(404).json({
+        message: `article with ${id} is not found `,
+      });
+    }
+    const { updatedContent, references } = req.body;
+    if ((!updatedContent, references)) {
+      return res.status(400).json({
+        message: "All the details are required",
+      });
+    }
+
+    article.updatedContent = updatedContent;
+    article.references = references;
+    article.isAiUpdated = isAiUpdated;
+
+    await article.save();
+
+    return res.status(200).json({
+      updatedArticle: article,
+      message: "Article updated ",
+    });
+  } catch (err) {
+    console.log("Error while updating the article by id: ", err.message);
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+exports.getArticleById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const article = await articleModel.findById(id);
+    if (!article) {
+      res.status(404).json({
+        message: "Article not found ",
+      });
+    }
+
+    return res.status(200).json({
+      article,
+      message: "Article fetched successfully ",
+    });
+  } catch (err) {
+    console.log("Error while getting article by id , error : ", error.message);
+    return res.status(500).json({
+      error: error.message,
     });
   }
 };
--- server/routes/article.routes.js
+++ server/routes/article.routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { initializeDatabase } = require("../controllers/article.controller");
+const {
+  initializeDatabase,
+  getAllArticles,
+  updateArticleById,
+  getArticleById,
+} = require("../controllers/article.controller");
 router = express.Router();
 
 router.post("/seed", initializeDatabase);
+router.get("/", getAllArticles);
+router.put("/:id", updateArticleById);
+router.get("/:id", getArticleById);
 
 module.exports = router;
