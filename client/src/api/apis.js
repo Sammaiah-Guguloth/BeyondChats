0d1e3b679cb842c61507a74fda04b463d5619844
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2025-12-31 22:21:25 +0530

    fixed the apis endpoints

--- client/src/api/apis.js
+++ client/src/api/apis.js
@@ -1,5 +1,5 @@
 //  =================== API END POINTS ===========
 
-export const GET_ALL_ARTICLES = "/articles"; // GET
-export const UPDATE_ARTICLE_BY_ID = "/articles"; // /:id // PUT
-export const GET_ARTICLE_BY_ID = "/articles"; // /:id // GET
+export const GET_ALL_ARTICLES = "api/v1/articles"; // GET
+export const UPDATE_ARTICLE_BY_ID = "api/v1/articles"; // /:id // PUT
+export const GET_ARTICLE_BY_ID = "api/v1/articles"; // /:id // GET
